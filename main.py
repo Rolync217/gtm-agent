6684775c4e10064a76a3d8747be7ddcc4e4c570e
Abhinav Anand <abhinavanand@Abhinavs-MacBook-Air-2.local>
2026-05-16 13:33:37 -0500

    Update email signature phone number

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ async def send_email_booking_link(to_email: str):
                         f"<p>Hey {DEMO_LEAD['name']},</p>"
                         f"<p>Great speaking with you! Here's your booking link for our 30-min discovery call:</p>"
                         f"<p><a href='{CAL_EVENT_URL}'>{CAL_EVENT_URL}</a></p>"
-                        f"<p>Talk soon,<br><strong>Abhinav Anand</strong><br>Founder & CEO, Rolync<br>+16514444766</p>"
+                        f"<p>Talk soon,<br><strong>Abhinav Anand</strong><br>Founder & CEO, Rolync<br>+16206999562</p>"
                     ),
                 },
             )
